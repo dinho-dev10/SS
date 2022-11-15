6b98d3472a22266339849c7498c4efeec77d0bc9
dinho-dev10 <108373753+dinho-dev10@users.noreply.github.com>
2022-11-15 17:20:24 +0000

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 const divMenu = document.querySelector(".menu.mobile.bg");
-
+const buttonCloseMenu = document.querySelector(".closeMenu");
 
 function openCloseMenu() {
     divMenu.classList.add("menu-active");
+}
+function closeMenu() {
+    divMenu.classList.remove("menu-active");
 }
